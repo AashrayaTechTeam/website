a48274bae479ffb1401f21ebab698fe05613ccbb
HimangshuB <baishyahimangshu499@gmail.com>
2022-01-30 18:18:27 +0000

    update

--- src/COMPONENTS/HOME__PAGE/SLIDESHOW/SlideShow.js
+++ src/COMPONENTS/HOME__PAGE/SLIDESHOW/SlideShow.js
@@ -32,13 +32,14 @@ let imageArray = [
 function SlideShow() {
   return (
     <section>
+    <div style={{background:"black" , width:"100%" , height:"600px" ,position:"absolute"}}></div>
       <Carousel interval={3000}>
         {imageArray.map((element, i) => {
           return (
             <Carousel.Item style={{ height: "600px" }}>
               <div
                 className="bg-image"
-                style={{ backgroundImage: `url(${element.src})` }}
+                style={{ backgroundImage: `url(${element.src})`, opacity:"0.5" }}
               />
               <div className="bg-background">
                 <div className="bg-textdiv">
