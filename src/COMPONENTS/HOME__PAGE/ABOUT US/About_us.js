3f36a8b984a5f0fc64d71ad87318a7ad1546ff5f
Kritika <rupatidarkritika363@gmail.com>
2022-02-05 12:56:13 +0530

    what aashraya do?

--- src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
+++ src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 function About_us() {
     return (
-        <div style={{width:"100%" , height:"400px" , background:"white" , color:"black" , textAlign:"center"}}>
-            <h1>About Aashraya</h1>
+        <><div style={{ width: "100%", height: "400px", background: "white", color: "black", textAlign: "center" }}>
+            {/* <h1>About Aashraya</h1> */}
+            <h1>What Aashraya Do ?</h1>
         </div>
+        <div className="box">
+            <div className="img">
+                <img src="images/logo.jpg" alt=""/>
+            </div>
+            <div className="content">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores soluta sit perspiciatis culpa quis nulla vel dicta maxime dolore optio minima officiis obcaecati aut, eius laborum corporis ducimus officia nisi voluptas voluptatibus blanditiis ea! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, atque.
+                </p>
+            </div>
+        </div></>
     )
 }
 
