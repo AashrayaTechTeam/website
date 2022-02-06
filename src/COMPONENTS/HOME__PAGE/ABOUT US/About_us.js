e1e78d2e7efc33f139d9b47851378af447f3cfd1
Kritika <rupatidarkritika363@gmail.com>
2022-02-06 16:16:14 +0530

    Added about_us.css

--- src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
+++ src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import logo from '../../../ASSETS/logo.jpg'
+import './About_us.css'
 
 function About_us() {
     return (
-        <><div style={{ width: "100%", height: "400px", background: "white", color: "black", textAlign: "center" }}>
-            {/* <h1>About Aashraya</h1> */}
-            <h1>What Aashraya Do ?</h1>
-        </div>
-        <div className="box">
+		<> <div className="heading">
+                <h1>What Aashraya Do ?</h1>
+			</div>
+        
+	    <div className="box">
             <div className="img">
-                <img src="images/logo.jpg" alt=""/>
-            </div>
+                <img src={logo} alt="Aashraya" style={{height: "400px", width: "400px", }}/>
+			</div> 
             <div className="content">
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores soluta sit perspiciatis culpa quis nulla vel dicta maxime dolore optio minima officiis obcaecati aut, eius laborum corporis ducimus officia nisi voluptas voluptatibus blanditiis ea! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, atque.
                 </p>
             </div>
-        </div></>
+		</div>
+		</>
     )
 }
 
