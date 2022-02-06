9b6377475c2ff15123940e562913ae2fc0a37641
Mani Prathi <prathinavya1414@gmail.com>
2022-02-06 09:17:04 +0000

    Added code for contactus

--- src/COMPONENTS/CONTACTUS/Contactus.js
+++ src/COMPONENTS/CONTACTUS/Contactus.js
@@ -3,27 +3,29 @@ import './contactus.css';
 
 function Contact_us() {
     return (
-        <div>
+        
+          <body className='body-contactus'>
+           <div>
            <h1 className='h1-contactus'>Contact US</h1>
       <p className="sub-title-contactus" >Any question or remarks? Just write us a message!</p>
 
-      <div className="contact-container">
+      <div className="contact-container-contactus">
         <div className="contact-info-contactus">
           <h4 className="h4-contactus">
             Contact Information
           </h4>
-          <p>Fill up the form and our team will get back</p>
+          <p className='p-contactus'>Fill up the form and our team will get back</p>
           <div className="icon-text-contactus">
             <i class="fa-contactus fa-phone" aria-hidden="true"></i>
-            <span>123456789</span>
+            <span className='span-contactus'>123456789</span>
           </div>
           <div className="icon-text-contactus">
             <i class="fa-contactus fa-envelope" aria-hidden="true"></i>
-            <span>addyourmail.gmail.com</span>
+            <span className='span-contactus'>addyourmail.gmail.com</span>
           </div>
           <div className="icon-text-contactus">
             <i class="fa-contactus fa-map-marker" aria-hidden="true"></i>
-            <span>Aashraya main office address</span>
+            <span className='span-contactus'>Aashraya main office address</span>
           </div>
           <div className="social-media-contactus">
             <a href="#" className="icon-circle-contactus">
@@ -44,42 +46,43 @@ function Contact_us() {
         <form>
           <div className="col-contactus">
             <div className="form-group-contactus">
-              <label>First Name</label>
-              <input type="text" />
+              <label className='label-contactus'>First Name</label>
+              <input className='input-contactus' type="text" />
             </div>
             <div className="form-group-contactus">
-              <label>Last Name</label>
-              <input type="text" />
+              <label className='label-contactus'>Last Name</label>
+              <input className='input-contactus'type="text" />
             </div>
           </div>
           <div className="col-contactus">
             <div className="form-group-contactus">
-              <label>E-Mail</label>
-              <input type="email" />
+              <label className='label-contactus'>E-Mail</label>
+              <input className='input-contactus' type="email" />
             </div>
             <div className="form-group-contactus">
-              <label>Phone</label>
-              <input type="tel" />
+              <label className='label-contactus'>Phone</label>
+              <input className='input-contactus' type="tel" />
             </div>
           </div>
           <div className="col-contactus">
             <div className="form-group-contactus">
-              <label>Write Your message </label>
+              <label className='label-contactus'>Write Your message </label>
 
-              <textarea-contactus></textarea-contactus>
+              <textarea className='textarea-contactus'></textarea>
 
             </div>
 
 
           </div>
-          <div className="form-group-contactus right">
-            <button-contactus className="Primary">Send message</button>
+          <div className="form-group-contactus right-contactus">
+            <button className="button-contactus">Send message</button>
 
           </div>
         </form>
       </div >
-
-        </div>
+      </div>
+      </body>
+       
     )
 }
 
