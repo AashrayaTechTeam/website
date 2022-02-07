562b6ae34fb3cb2a061d612ce403348f1421e8a5
HimangshuB <baishyahimangshu499@gmail.com>
2022-02-07 05:29:23 +0000

    update

--- src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
+++ src/COMPONENTS/HOME__PAGE/ABOUT US/About_us.js
@@ -5,16 +5,20 @@ import './About_us.css'
 function About_us() {
     return (
 		<> <div className="heading">
-                <h1>What Aashraya Do ?</h1>
+                <h1>What Aashraya Does ?</h1>
 			</div>
         
 	    <div className="box">
             <div className="img">
-                <img src={logo} alt="Aashraya" style={{height: "400px", width: "400px", }}/>
+                <img src={logo} alt="Aashraya"/>
 			</div> 
             <div className="content">
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores soluta sit perspiciatis culpa quis nulla vel dicta maxime dolore optio minima officiis obcaecati aut, eius laborum corporis ducimus officia nisi voluptas voluptatibus blanditiis ea! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, atque.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores 
+                    soluta sit perspiciatis culpa quis nulla vel dicta maxime dolore 
+                    optio minima officiis obcaecati aut, eius laborum corporis ducimus 
+                    officia nisi voluptas voluptatibus blanditiis ea! Lorem ipsum dolor 
+                    sit amet, consectetur adipisicing elit. Veniam, atque.
                 </p>
             </div>
 		</div>
