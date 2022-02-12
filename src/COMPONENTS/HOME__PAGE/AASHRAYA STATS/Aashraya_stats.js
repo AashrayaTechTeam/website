6d36cb0d3c2d660a1e62654f772ba7fd5c55c0b8
HimangshuB <baishyahimangshu499@gmail.com>
2022-02-12 07:48:31 +0000

    update

--- src/COMPONENTS/HOME__PAGE/AASHRAYA STATS/Aashraya_stats.js
+++ src/COMPONENTS/HOME__PAGE/AASHRAYA STATS/Aashraya_stats.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import books from '../../../ASSETS/statistics/books.png'
+import library from '../../../ASSETS/statistics/library.png'
+import volunteers from '../../../ASSETS/statistics/volunteers.png'
 function Aashraya_stats() {
   return (
-    <div style={{height:"300px" , textAlign:"center"}}><h1>Aashraya Stats</h1></div>
+    <div style={{textAlign:"center"}}>
+      <h1>Aashraya Stats</h1>
+    <div style={{height:"500px"  , display:"flex" , justifyContent:"center" }}>
+      <div style={{margin:"100px" , display:"flex" , flexDirection:"column", textAlign:"center"}}>
+          <img src={volunteers} alt='icon'/>
+          <div style={{margin:"30px 10px"}}>
+            <h3><strong>Total Volunteers</strong></h3>
+            <h3>3000+ </h3>
+          </div>
+      </div>
+
+      <div style={{margin:"100px" , display:"flex" , flexDirection:"column", textAlign:"center"}}>
+        <img src={library} alt='icon'/>
+          <div style={{margin:"30px 10px"}}>
+            <h3><strong>Total Libraries</strong></h3>
+            <h3>10+</h3>
+          </div>
+      </div>
+
+      <div style={{margin:"100px" , display:"flex" , flexDirection:"column", textAlign:"center"}}>
+          <img src={books} alt='icon'/>
+          <div style={{margin:"30px 10px"}}>
+            <h3><strong>Total Books</strong></h3>
+            <h3>100k+ </h3>
+          </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
--- src/COMPONENTS/HOME__PAGE/SOCIAL_MEDIA/Social_media.js
+++ src/COMPONENTS/HOME__PAGE/SOCIAL_MEDIA/Social_media.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import instagram from '../../../ASSETS/social/insta.png'
+import facebook from '../../../ASSETS/social/facebook.png'
+import linkedin from '../../../ASSETS/social/linkedin.png'
+import youtube from '../../../ASSETS/social/youtube.png'
 function Social_media() {
   return (
-    <div style={{width:"100%" , height:"300px",textAlign:"center"}}><h1>Social Media Handles</h1>
-    <img src ="" alt="Instagram"/>
-    <img src ="" alt="Instagram"/>
-    <img src ="" alt="Instagram"/>
-    <img src ="" alt="Instagram"/>
-    <img src ="" alt="Instagram"/>
+    <div style={{width:"100%" , height:"300px",textAlign:"center" , margin:"60px 0px"}}>
+      <h1 style={{margin:"60px"}}>Social Media Handles</h1>
+    <div>
+      <img src ={instagram} alt="Instagram"/>
+      <img src ={facebook} alt="Instagram"/>
+      <img src ={linkedin} alt="Instagram"/>
+      <img src ={youtube} alt="Instagram"/>
+    </div>
     </div>
     )
 }
