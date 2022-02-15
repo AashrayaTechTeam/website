3c849e39f57f421070f4d33a8fb86c894a460d72
Jayanthee365 <jayanthi.naik@gupshup.io>
2022-02-15 09:37:16 +0530

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,76 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import $ from "jquery";
-import "../../styles/Navbar/Navbar.css";
-import donate from "../../ASSETS/donate-vector.png";
+import React , {useState , useEffect} from 'react'
+import { Link ,useHistory} from 'react-router-dom'
+import $ from 'jquery'
+import '../../styles/Navbar/Navbar.css'
+import donate from "../../ASSETS/donate-vector.png"
 function Navbar() {
-  window.addEventListener("scroll", function () {
+
+
+  window.addEventListener('scroll' , function(){
     let res = window.scrollY;
-    if (res >= 150)
-      $("#navbar").css({
-        backgroundColor: "rgba(0, 0, 0, 0.884)",
-        padding: "10px 20px",
-        transition: "all 1s",
-      });
+    if(res >= 150)
+      $("#navbar").css({'backgroundColor':'rgba(0, 0, 0, 0.884)' , 'padding':'10px 20px' , 'transition':'all 1s' })  
     else
-      $("#navbar").css({
-        backgroundColor: "transparent",
-        padding: "40px 60px",
-      });
-  });
+      $("#navbar").css({'backgroundColor':'transparent' , 'padding':'40px 60px' })
+    
+  
+  })
+
 
   return (
     <div>
-      <nav
-        id="navbar"
-        className="navbar"
-        style={{
-          background: "transparent",
-          padding: "40px",
-          width: "100%",
-          height: "70px",
-          color: "white",
-          position: "fixed",
-          top: "0%",
-          left: "0%",
-          zIndex: "5",
-        }}
-      >
-        <div class="nav-logo">
-          <h3 class="logo">Aashraya</h3>
-        </div>
+      <nav id="navbar" className='navbar' style={{background:"transparent" , padding:"40px", width:"100%", height:"70px",
+    color:"white" , position:"fixed" , top:"0%" , left:"0%" , zIndex:"5"
+    }}>
+         <div class="nav-logo"><h3 class="logo">Aashraya</h3></div>
 
-        <ul
-          class="nav-links"
-          id="links"
-          style={{ position: "relative", right: "3%" }}
-        >
-          <li>
-            <a href="#">Team</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Join Us</a>
-          </li>
-          <li>
-            <a href="#">Library</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Gallery</a>
-          </li>
-          <li>
-            <a href="#">Donate</a>
-          </li>
-          <img src={donate} alt="..." />
-        </ul>
+<ul class="nav-links" id="links" style={{position:"relative" , right:"3%"}}>
+  <li><a href="#">Team</a></li>
+  <li><a href="#">Blog</a></li>
+  <li><a href="#">Join Us</a></li>
+  <li><a href="#">Library</a></li>
+  <li><a href="#">Projects</a></li>
+  <li><a href="#">Gallery</a></li>
+  <li><a href="#">Donate</a></li>
+  <img src={donate} alt="..."/>
+</ul>
       </nav>
     </div>
-  );
+
+  )
 }
 
-export default Navbar;
+export default Navbar
