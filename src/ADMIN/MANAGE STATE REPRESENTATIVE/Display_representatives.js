5c867af9502781235ba3a171cba26df0c8bd2883
Jayanthee365 <jayanthi.naik@gupshup.io>
2022-02-20 21:50:44 +0530

    fix

--- src/ADMIN/MANAGE STATE REPRESENTATIVE/Display_representatives.js
+++ src/ADMIN/MANAGE STATE REPRESENTATIVE/Display_representatives.js
@@ -1,45 +1,99 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getVolunteers } from "../../API/API";
 
 function Display_representatives() {
-    return (
-        <div>
-            <div style={{ display:"flex" , border:"solid 2px " , margin:"10px" ,padding:"10px"}} >
-                <img style={{width:"20rem" , height:"25rem"}} src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80" class="card-img-top" alt="PHOTO"/>
-                <div class="card-body">
-                    <h5 class="card-title">Name</h5>
-                    <p class="card-text">CITY</p>
-                    <p class="card-text">ABOUT</p>
-                    <p class="card-text">CONTACT</p>
-                    <p class="card-text">SOCIAL MEDIA</p>
-                    <p class="card-text">EMAIL</p>
-                   
-                </div>
-            </div>
-            <div style={{ display:"flex" , border:"solid 2px " , margin:"10px"  , padding:"10px"}} >
-                <img style={{width:"20rem" , height:"25rem"}} src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80" class="card-img-top" alt="PHOTO"/>
-                <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                    <p class="card-text">CITY</p>
-                    <p class="card-text">ABOUT</p>
-                    <p class="card-text">CONTACT</p>
-                    <p class="card-text">SOCIAL MEDIA</p>
-                    <p class="card-text">EMAIL</p>
-                </div>
-            </div>
-            <div style={{ display:"flex" , border:"solid 2px " , margin:"10px" ,padding:"10px"}} >
-                <img style={{width:"20rem" , height:"25rem"}} src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80" class="card-img-top" alt="PHOTO"/>
-                <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                    <p class="card-text">CITY</p>
-                    <p class="card-text">ABOUT</p>
-                    <p class="card-text">CONTACT</p>
-                    <p class="card-text">SOCIAL MEDIA</p>
-                    <p class="card-text">EMAIL</p>
-                </div>
-            </div>
+  const [volunteer, setVolunteers] = useState([]);
+  useEffect(() => {
+    getVolunteer();
+  }, []);
 
+  const getVolunteer = () => {
+    getVolunteers("volunteers")
+      .then((res) => {
+        setVolunteers(res);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+  return (
+    <div>
+      {/* {volunteer.map((element) => {
+        return <p>{JSON.stringify(element)}</p>;
+      })} */}
+      <div
+        style={{
+          display: "flex",
+          border: "solid 2px ",
+          margin: "10px",
+          padding: "10px",
+        }}
+      >
+        <img
+          style={{ width: "20rem", height: "25rem" }}
+          src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+          class="card-img-top"
+          alt="PHOTO"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Name</h5>
+          <p class="card-text">CITY</p>
+          <p class="card-text">ABOUT</p>
+          <p class="card-text">CONTACT</p>
+          <p class="card-text">SOCIAL MEDIA</p>
+          <p class="card-text">EMAIL</p>
         </div>
-    )
+      </div>
+      <div
+        style={{
+          display: "flex",
+          border: "solid 2px ",
+          margin: "10px",
+          padding: "10px",
+        }}
+      >
+        <img
+          style={{ width: "20rem", height: "25rem" }}
+          src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+          class="card-img-top"
+          alt="PHOTO"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Name</h5>
+          <p class="card-text">CITY</p>
+          <p class="card-text">ABOUT</p>
+          <p class="card-text">CONTACT</p>
+          <p class="card-text">SOCIAL MEDIA</p>
+          <p class="card-text">EMAIL</p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          border: "solid 2px ",
+          margin: "10px",
+          padding: "10px",
+        }}
+      >
+        <img
+          style={{ width: "20rem", height: "25rem" }}
+          src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+          class="card-img-top"
+          alt="PHOTO"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Name</h5>
+          <p class="card-text">CITY</p>
+          <p class="card-text">ABOUT</p>
+          <p class="card-text">CONTACT</p>
+          <p class="card-text">SOCIAL MEDIA</p>
+          <p class="card-text">EMAIL</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Display_representatives
+export default Display_representatives;
