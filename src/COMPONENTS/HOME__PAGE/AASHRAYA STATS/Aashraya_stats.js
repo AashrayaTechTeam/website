357d9a95b5c74f62bbe1524974d09b6a37e491cd
Kritika <rupatidarkritika363@gmail.com>
2022-02-21 11:47:37 +0530

    update

--- src/COMPONENTS/HOME__PAGE/AASHRAYA STATS/Aashraya_stats.js
+++ src/COMPONENTS/HOME__PAGE/AASHRAYA STATS/Aashraya_stats.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import books from '../../../ASSETS/statistics/books.png'
 import library from '../../../ASSETS/statistics/library.png'
 import volunteers from '../../../ASSETS/statistics/volunteers.png'
 import '../../../styles/Aashraya_stats.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 function Aashraya_stats() {
+  useEffect(() => {
+    Aos.init({ duration: 3000 });
+}, []) ;
+
   return (
     <div style={{textAlign:"center", paddingTop:"240px",color:"black"}}>
-      <h1>Aashraya Stats</h1>
+      <h1 data-Aos="fade-down" >Aashraya Stats</h1>
     <div style={{height:"500px"  , display:"flex" , justifyContent:"center" }}>
-      <div className="icon-stats">
+      <div className="icon-stats" data-Aos="fade-right">
           <img src={volunteers} alt='icon'/>
           <div style={{margin:"30px 10px"}}>
             <h3><strong>Total Volunteers</strong></h3>
@@ -16,7 +23,7 @@ function Aashraya_stats() {
           </div>
       </div>
 
-      <div className="icon-stats">
+      <div className="icon-stats" data-Aos="fade-up">
         <img src={library} alt='icon'/>
           <div style={{margin:"30px 10px"}}>
             <h3><strong>Total Libraries</strong></h3>
@@ -24,7 +31,7 @@ function Aashraya_stats() {
           </div>
       </div>
 
-      <div className='icon-stats'>
+      <div className='icon-stats' data-Aos="fade-left">
           <img src={books} alt='icon'/>
           <div style={{margin:"30px 10px"}}>
             <h3><strong>Total Books</strong></h3>
