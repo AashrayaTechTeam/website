18d03f7001c478093528627a27a01e5433f59d7a
Devanshi Parikh <96438552+DParikh19@users.noreply.github.com>
2022-03-09 19:00:55 +0530

    Revert "Responsive-website"

--- src/Temp Component/Sakti Week/SaktiWeek.js
+++ src/Temp Component/Sakti Week/SaktiWeek.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "../../styles/SaktiWeek.css";
+import React from 'react'
+import '../../styles/SaktiWeek.css'
 function SaktiWeek() {
   return (
     <main>
-      <div className="sakti-container">
-        <div>
-          <h1>Coming Soon</h1>
+        <div className='sakti-container'>
+            <div>
+                <h1>Coming Soon</h1>
+            </div>
         </div>
-      </div>
     </main>
-  );
+  )
 }
 
-export default SaktiWeek;
+export default SaktiWeek
